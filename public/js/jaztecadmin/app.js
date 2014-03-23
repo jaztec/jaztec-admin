7bb68f87ab5af1cb8269462d3db022325f2994d4
Jasper van Herpt <jasper@jaspervanherpt.com>
2014-03-23 19:08:25 +0100

    Tweak jslint

--- public/js/jaztecadmin/app.js
+++ public/js/jaztecadmin/app.js
@@ -1,5 +1,5 @@
 /*globals Ext, JaztecAdminApp*/
 Ext.require('JaztecAdmin.app.Application');
 if (JaztecAdminApp === undefined) {
-    var JaztecAdminApp = Ext.create('JaztecAdmin.app.Application');
+    JaztecAdminApp = Ext.create('JaztecAdmin.app.Application');
 }
