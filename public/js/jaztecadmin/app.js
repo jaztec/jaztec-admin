36d0bf56128ec7cd771d32c1d732f30a40ddbf5a
Jasper van Herpt <jasper@jaspervanherpt.com>
2014-03-23 19:14:44 +0100

    Reverted to start, that works.

--- public/js/jaztecadmin/app.js
+++ public/js/jaztecadmin/app.js
@@ -1,6 +1,5 @@
 /*globals Ext, JaztecAdminApp*/
-var JaztecAdminApp = JaztecAdminApp || {};
 Ext.require('JaztecAdmin.app.Application');
-if (JaztecAdminApp === {}) {
-    JaztecAdminApp = Ext.apply(JaztecAdminApp, Ext.create('JaztecAdmin.app.Application'));
+if (JaztecAdminApp === undefined) {
+    var JaztecAdminApp = Ext.create('JaztecAdmin.app.Application');
 }
