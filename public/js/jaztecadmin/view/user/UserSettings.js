1cd1f4e909a8372fef5cdbf9040141752dddd890
Jasper van Herpt <jasper@jaspervanherpt.com>
2014-06-01 15:58:59 +0200

    Upped the user settings part

--- public/js/jaztecadmin/view/user/UserSettings.js
+++ public/js/jaztecadmin/view/user/UserSettings.js
@@ -12,14 +12,38 @@ Ext.define('JaztecAdmin.view.user.UserSettings', {
     width: 800,
     height: 600,
 
+    layout: {
+        type: 'fit',
+        align: 'stretch'
+    },
+
     initComponent: function()
     {
         var me = this,
-            form = this.createForm();
+            form = this.createForm(),
+            toolbar = this.createToolbar();
+
+        this.addEvents(
+            /**
+             * @event save-user-settings
+             * Fired when the user settings are saved.
+             * @param {JaztecAdmin.view.user.UserSettings} this
+             * @param {Array} formValues
+             */
+            'save-user-settings',
+            /**
+             * @event cancel-user-settings
+             * Fired when the user settings are cancelled.
+             * @param {JaztecAdmin.view.user.UserSettings} this
+             */
+            'cancel-user-settings'
+        );
 
         this.items = [
             form
         ];
+
+        this.bbar = toolbar;
 
         this.callParent(arguments);
     },
@@ -30,17 +54,99 @@ Ext.define('JaztecAdmin.view.user.UserSettings', {
      */
     createForm: function()
     {
-        var me = this,
-            form;
+        var form;
         form = Ext.create('Ext.form.Panel', {
+            bodyPadding: 5,
             border: false,
-            frame: false,
+            trackResetOnLoad: true,
+            defaultType: 'textfield',
+            layout: 'form',
+            fieldDefaults: {
+                labelWidth: 125,
+                msgTarget: 'side',
+                align: 'stretch'
+            },
             items: [
                 {
                     fieldLabel: 'Username',
-                    name: 'username'
+                    name: 'username',
+                    allowBlank: false
+                },
+                {
+                    fieldLabel: 'Display name',
+                    name: 'displayName',
+                    allowBlank: false
                 }
             ]
         });
+        return form;
+    },
+
+    /**
+     * Creates a toolbar inside the window.
+     * 
+     * @returns {Ext.toolbar.Toolbar}
+     * @private
+     */
+    createToolbar: function()
+    {
+        var me = this,
+            toolbar;
+        toolbar = Ext.create('Ext.toolbar.Toolbar', {
+            items: [
+                '->',
+                {
+                    text: 'Save',
+                    handler: Ext.bind(me.fireSaveEvent, me)
+                },
+                {
+                    text: 'Cancel',
+                    handler: Ext.bind(me.fireCancelEvent, me)
+                },
+                {
+                    text: 'Close',
+                    handler: Ext.bind(me.closeWindow, me)
+                }
+            ]
+        });
+        return toolbar;
+    },
+
+    /**
+     * Validates the form and fires the save event when valid.
+     * @returns {Boolean}
+     * @private
+     */
+    fireSaveEvent: function()
+    {
+        var form = this.down('form');
+        if (!form.isValid()) {
+            return false;
+        }
+        this.fireEvent('save-user-settings', this, form.getValues());
+        return true;
+    },
+
+    /**
+     * Fires the cancel event and resets the form.
+     * @todo Reset the form
+     * @returns {JaztecAdmin.view.user.UserSettings} this
+     * @private
+     */
+    fireCancelEvent: function()
+    {
+        var form = this.down('form');
+        form.getForm().reset();
+        this.fireEvent('cancel-user-settings', this);
+        return this;
+    },
+
+    /**
+     * Closes this window
+     * @returns {undefined}
+     */
+    closeWindow: function()
+    {
+        this.close();
     }
 });
